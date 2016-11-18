164d2823a76423d531c0786aea088ca2d2915ce9
David Audet <david.michael.audet@gmail.com>
2016-11-18 12:26:16 -0800

    Adding setuptools and initial implementation for encode/decode

---
+++ setup.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2016 David Audet
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup
+
+setup(
+    name='pyb64',
+    version='0.1',
+    py_modules=['pyb64'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        pyb64=pyb64:pyb64
+    ''',
+)
